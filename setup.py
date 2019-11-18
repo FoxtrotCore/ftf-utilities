f98caf2451c6defe99b9aefcc8c0cfc6deb5fe9d
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2019-11-18 12:17:41 -0800

    forgot versioning...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ftfutils",
-    version="2.0.0",
+    version="2.0.1",
     author="Foxtrot Fanatics",
     author_email="foxtrotfanatics@gmail.com",
     packages=['ftfutils'],
